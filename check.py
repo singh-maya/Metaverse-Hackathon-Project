f9302a557d6ab3ca08b82e4ec5325d512c2fc010
Lasyasri Shilpi <shilpilasya0@gmail.com>
2024-08-22 19:26:27 +0000

    login stuff

---
+++ check.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv(filepath_or_buffer='Parsed Datasets/Metaverse.csv')
+df = pd.DataFrame(data)
+
+color_map = {    'low_risk': 'green',    'moderate_risk': 'orange',    'high_risk': 'red'}#
+#Plotting the scatter plot
+plt.figure(figsize=(10, 6))
+for anomaly_type,color in color_map.items():
+    subset =  df[df['anomaly'] == anomaly_type]   
+    plt.scatter(subset['login_frequency'], subset['session_duration'], c=color, label=anomaly_type, s=100)
+
+plt.title('Scatter Plot of Risk correlated to Frequency and Duration')
+plt.xlabel('Login Frequency')
+plt.ylabel('Session Duration (minutes)')
+plt.legend(title='Anomaly')
+plt.grid(True)
+# Show the scatter plot in Streamlitstequency with Anomaly")st.pyplot(ation vs Login Frequency with Anomaly")
+st.pyplot(plt)
+# Plotting the scatter plot
+
